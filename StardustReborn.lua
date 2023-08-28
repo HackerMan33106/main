a379df18582041d556ed1393e59d8ce85548117f
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2023-08-28 22:43:37 +0300

    Update StardustReborn.lua

--- StardustReborn.lua
+++ StardustReborn.lua
@@ -6,16 +6,22 @@ local Tab = Window:NewTab("Teleporting")
 local Section = Tab:NewSection("Teleport")
 
 -- Teleport to bosses
-Section:NewDropdown("Teleport to bosses", "You teleport to the bosses.", {"DIO", "Diavolo"}, function(teleport)
-    if teleport == "DIO" then
+Section:NewDropdown("Teleport to bosses", "You teleport to the bosses.", {"DIO", "Diavolo", "Jotaro P3", "Chaka", "Goku"}, function(teleport)
+if teleport == "DIO" then
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.Alive.DIO.HumanoidRootPart.CFrame
 elseif teleport == "Diavolo" then
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.Alive.Diavolo.HumanoidRootPart.CFrame
+elseif teleport == "Jotaro P3" then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.Alive["Jotaro P3"].HumanoidRootPart.CFrame
+elseif teleport == "Chaka" then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.Alive.Chaka.HumanoidRootPart.CFrame
+elseif teleport == "Goku" then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.Alive.Goku.HumanoidRootPart.CFrame
     end
 end)
 
 -- Teleport to NPC
-Section:NewDropdown("Teleport to NPC", "You teleport to the NPC.", {"Enrico Pucci", "Jotaro", "VerifiedPixel", "Giorno Giovanna", "Becca", "TheForgotten0range", "k7rdan", "Sans"}, function(npc)
+Section:NewDropdown("Teleport to NPC", "You teleport to the NPC.", {"Enrico Pucci", "Jotaro", "VerifiedPixel", "Giorno Giovanna", "Becca", "TheForgotten0range", "k7rdan", "Shadow DIO", "Goku"}, function(npc)
     if npc == "Enrico Pucci" then
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.NPC["Enrico Pucci"].HumanoidRootPart.CFrame
 elseif npc == "Jotaro" then
@@ -30,8 +36,10 @@ elseif npc == "TheForgotten0range" then
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.NPC.TheForgotten0range.HumanoidRootPart.CFrame
 elseif npc == "k7rdan" then
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.NPC.k7rdan.HumanoidRootPart.CFrame
-elseif npc == "Sans" then
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.NPC.Sans.HumanoidRootPart.CFrame
+elseif npc == "Shadow DIO" then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.NPC["Shadow DIO"].HumanoidRootPart.CFrame
+elseif npc == "Goku" then
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.NPC.Goku.HumanoidRootPart.CFrame
     end
 end)
 
@@ -53,26 +61,42 @@ elseif dummy == "Barraging Dummy" then
 end)
 
 -- Teleport to players
-Section:NewTextBox("Teleport to players", "Enter a nickname for the person you want to teleport to. (If it doesn't teleport, you entered the wrong nickname.)", function(ply)
+Section:NewTextBox("Teleport to players", "Enter nickname for teleport(wrong nickname, no teleport).", function(ply)
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.Alive[ply].HumanoidRootPart.CFrame
 end)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --- 2 Section Bosses
 local Tab = Window:NewTab("Bosses")
 local Section = Tab:NewSection("Bosses management")
 
 -- Instant kill bosses
-Section:NewDropdown("Instant kill bosses", "You instantly kill bosses.", {"DIO", "Diavolo"}, function(kill)
-    if kill == "DIO" then
+Section:NewDropdown("Instant kill bosses", "You instantly kill bosses.", {"DIO", "Diavolo", "Jotaro P3", "Chaka", "Goku"}, function(kill)
+if kill == "DIO" then
         game:GetService("Workspace").Game.Alive.DIO.Humanoid.RigType = 1
         wait(1)
         game:GetService("Workspace").Game.Alive.DIO.Humanoid.RigType = 0
-elseif kill == "Diavolo" then 
+elseif kill == "Diavolo" then
     game:GetService("Workspace").Game.Alive.Diavolo.Humanoid.RigType = 1
     wait(1)
     game:GetService("Workspace").Game.Alive.Diavolo.Humanoid.RigType = 0
+elseif kill == "Jotaro P3" then
+    game:GetService("Workspace").Game.Alive["Jotaro P3"].Humanoid.RigType = 1
+    wait(1)
+    game:GetService("Workspace").Game.Alive["Jotaro P3"].Humanoid.RigType = 0
+elseif kill == "Chaka" then
+    game:GetService("Workspace").Game.Alive.Chaka.Humanoid.RigType = 1
+    wait(1)
+    game:GetService("Workspace").Game.Alive.Chaka.Humanoid.RigType = 0
+elseif kill == "Goku" then
+    game:GetService("Workspace").Game.Alive.Goku.Humanoid.RigType = 1
+    wait(1)
+    game:GetService("Workspace").Game.Alive.Goku.Humanoid.RigType = 0
     end
 end)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --- 3 Section Teleport to Items
 local Tab = Window:NewTab("Teleporting to items")
@@ -88,6 +112,7 @@ Section:NewButton("Teleport to rokakaka", "You teleport to rokakaka .", function
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Game.Items.Roka.Handle.CFrame
 end)
 
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --- 4 Section Misc
 local Tab = Window:NewTab("Misc")
@@ -115,17 +140,49 @@ end)
 
 -- Noclip
 Section:NewButton("Noclip on", "You can walk through walls, objects, etc.", function()
-    while game:GetService("RunService").RenderStepped:wait() do
-    game.Players.LocalPlayer.Character.Head.CanCollide = false
-    game.Players.LocalPlayer.Character.Torso.CanCollide = false
-    if game.Players.LocalPlayer.Character.Torso == true then
-        game.Players.LocalPlayer.Character.Head.CanCollide = false
+        while game:GetService("RunService").RenderStepped:wait() do
+            game.Players.LocalPlayer.Character.Head.CanCollide = false
+            if game.Players.LocalPlayer.Character.Head.CanCollide == true then
+            game.Players.LocalPlayer.Character.Head.CanCollide = false
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CamCollide = false
+            if game.Players.LocalPlayer.Character.HumanoidRootPart.CamCollide == true then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CamCollide = false
+            game.Players.LocalPlayer.Character.Torso.CanCollide = false
+            if game.Players.LocalPlayer.Character.Torso == true then
+            game.Players.LocalPlayer.Character.Torso.CanCollide = false
+                end
+            end
         end
     end
 end)
 
-Section:NewButton("Noclip off", "You stop walking through walls, objects, etc.", function()
-    game.Players.LocalPlayer.Character.Humanoid.RigType = 1
+-- ESP
+Section:NewButton("ESP", "You can see the players through the wall.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/ESP/main/ESP.lua", true))()
+end)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- 5 Section Fun
+-- Fun
+local Tab = Window:NewTab("Fun")
+local Section = Tab:NewSection("Fun")
+
+-- Undressing the players
+Section:NewTextBox("Undressing the plr", "Enter nickname to undress(wrong nickname = no undressing).", function(utp)
+    game:GetService("Workspace").Game.Alive[utp].Shirt:Destroy()
+	game:GetService("Workspace").Game.Alive[utp].Pants:Destroy()
+    game:GetService("Workspace").Game.Alive[utp]["Shirt Graphic"]:Destroy()
+end)
+
+-- Field Of Viev
+Section:NewSlider("Field Of Viev", "You can control your field of viev.", 120, 1, function(fov)
+    game:GetService("Workspace").Camera.FieldOfView = fov
+end)
+
+-- Default field of viev
+Section:NewButton("Default field of viev", "You set Default field of view.", function()
+    game:GetService("Workspace").Camera.FieldOfView = 70
 end)
 
 -- Sit
@@ -133,11 +190,41 @@ Section:NewKeybind("Sit", "You sit down.", Enum.KeyCode.V, function()
 	game.Players.LocalPlayer.Character.Humanoid.Sit = true 
 end)
 
---- 5 Section Credit
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- 6 Section Other scripts
+-- Other scripts
+local Tab = Window:NewTab("Other scripts")
+local Section = Tab:NewSection("Scripts")
+
+-- DarkDex V4
+Section:NewButton("Inject DarkDex V4", "You inject DarkDex V4.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+end)
+
+-- Infinite Yeild
+Section:NewButton("Inject Infinite Yeild", "You inject Infinite Yeild.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/Infinite-yeild/main/Infiniteyeild.lua", true))()
+end)
+
+-- CMD-X
+Section:NewButton("Inject CMD-X", "You inject CMD-X.", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/CMD-X/main/CMD-X.lua", true))()
+end)
+
+-- Your script
+Section:NewTextBox("Your script", "Paste the link to your script here.", function(uscript)
+	loadstring(game:HttpGet(uscript, true))()
+end)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- 7 Section Credit
 -- Credit
 local Tab = Window:NewTab("Credit")
-local Section = Tab:NewSection("By HackerMan")
+local Section = Tab:NewSection("By HackerMan33105")
 
+Section:NewLabel("Helped with script ChatGPT")
 Section:NewLabel("Made according to the Robojini guide")
 Section:NewButton("His discord", "Click to copy the link.", function()
     setclipboard("https://discord.gg/E4BdnAXsuE")
