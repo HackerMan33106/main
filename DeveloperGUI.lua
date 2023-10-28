4e0a06833679393690bd0315185e53c011c3d261
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2023-10-28 13:15:43 +0300

    Add files via upload

---
+++ DeveloperGUI.lua
@@ -0,0 +1,6 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+local Window = Library.CreateLib("Heaven Stand Developer GUI", "RJTheme1")
+
+--- 1 Section Test Function
+local Tab = Window:NewTab("Test functions")
+local Section = Tab:NewSection("Test functions")
