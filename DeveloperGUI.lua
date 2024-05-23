f3d279317c5609c36ed5881f55ede69521a7474e
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2024-05-23 19:41:08 +0300

    Update DeveloperGUI.lua

--- DeveloperGUI.lua
+++ DeveloperGUI.lua
@@ -1,6 +1,9 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 local Window = Library.CreateLib("Heaven Stand Developer GUI", "RJTheme1")
 
+--[[
 --- 1 Section Test Function
 local Tab = Window:NewTab("Test functions")
 local Section = Tab:NewSection("Test functions")
+]]
+game.Players.LocalPlayer:Kick("Going through other people's scripts is bad.")
