ce504cbe6c96f57c9bc680b383524de8ffa5d791
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2024-05-24 00:35:09 +0300

    Update PInfo.lua

--- PInfo.lua
+++ PInfo.lua
@@ -1,7 +1,54 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Player info", "RJTheme2")
+local Window = Library.CreateLib("Player infomation", "RJTheme3")
 
-local Tab = Window:NewTab("Player Infomation")
-local Section = Tab:NewSection("Player Info")
+local PlayerName = game:GetService("Workspace").NameValue.Value
 
-Section:NewLabel()
+local Tab = Window:NewTab("Stand")
+local stand = game.Players.LocalPlayer.Settings.Data.Ability.Value
+Section:NewLabel(stand)
+
+local Tab = Window:NewTab("Cash")
+local cash = game.Players.LocalPlayer.Settings.Data.Cash.Value
+Section:NewLabel(cash)
+
+local Tab = Window:NewTab("Stand in storage")
+local StandInStorage1 = game.Players.Localplayer.Data["Ability Storage"].Ability_Slot1.Value
+local StandInStorage2 = game.Players.Localplayer.Data["Ability Storage"].Ability_Slot2.Value
+local StandInStorage3 = game.Players.Localplayer.Data["Ability Storage"].Ability_Slot3.Value
+local StandInStorage4 = game.Players.Localplayer.Data["Ability Storage"].Ability_Slot4.Value
+local StandInStorage5 = game.Players.Localplayer.Data["Ability Storage"].Ability_Slot5.Value
+local StandInStorage6 = game.Players.Localplayer.Data["Ability Storage"].Ability_Slot6.Value
+Section:NewLabel(StandInStorage1)
+Section:NewLabel(StandInStorage2)
+Section:NewLabel(StandInStorage3)
+Section:NewLabel(StandInStorage4)
+Section:NewLabel(StandInStorage5)
+Section:NewLabel(StandInStorage6)
+
+local Tab = Window:NewTab("Item in storage")
+local ItemInStorage1 = game.Players.Localplayer.Data["Item Storage"].Item_Slot1.Value
+local ItemInStorage2 = game.Players.Localplayer.Data["Item Storage"].Item_Slot2.Value
+local ItemInStorage3 = game.Players.Localplayer.Data["Item Storage"].Item_Slot3.Value
+local ItemInStorage4 = game.Players.Localplayer.Data["Item Storage"].Item_Slot4.Value
+local ItemInStorage5 = game.Players.Localplayer.Data["Item Storage"].Item_Slot5.Value
+local ItemInStorage6 = game.Players.Localplayer.Data["Item Storage"].Item_Slot6.Value
+Section:NewLabel(ItemInStorage1)
+Section:NewLabel(ItemInStorage2)
+Section:NewLabel(ItemInStorage3)
+Section:NewLabel(ItemInStorage4)
+Section:NewLabel(ItemInStorage5)
+Section:NewLabel(ItemInStorage6)
+
+local Tab = Window:NewTab("Settings")
+local Camera_Shake = game.Players.Localplayer.Settings["Camera Shake"].Value
+local CameraShake = game.Players.Localplayer.Settings.CameraShake.Value
+local LowGraphics = game.Players.Localplayer.Settings["Low Graphics"].Value
+local Music = game.Players.Localplayer.Settings.Music.Value
+Section:NewLabel(Camera_Shake)
+Section:NewLabel(CameraShake)
+Section:NewLabel(LowGraphics)
+Section:NewLabel(Music)
+
+local Tab = Window:NewTab("Leaderstats")
+local leaderstats = game.Players.Localplayer.leaderstats["Total Kill"].Value
+Section:NewLabel(leaderstats)
