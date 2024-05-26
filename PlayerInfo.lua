0d675f19d9dcd6e12ac133154ead2939989aad2b
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2024-05-26 21:43:50 +0300

    Update PlayerInfo.lua

--- PlayerInfo.lua
+++ PlayerInfo.lua
@@ -3,24 +3,33 @@ local Window = Library.CreateLib("Player infomation", "RJTheme3")
 
 local PlayerName = game:GetService("Workspace").NameValue.Value
 
-local Tab = Window:NewTab("Stand")
-local Section = Tab:NewSection("")
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local stand = game.Players[PlayerName].Settings.Data.Ability.Value
+local Tab = Window:NewTab("Selected player")
+local Section = Tab:NewSection("Selected player")
+
+Section:NewLabel(PlayerName)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+local Tab = Window:NewTab("Stand")
+local Section = Tab:NewSection("Stand")
+
+local stand = game.Players[PlayerName].Data.Ability.Value
 Section:NewLabel(stand)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 local Tab = Window:NewTab("Cash")
-local Section = Tab:NewSection("")
+local Section = Tab:NewSection("Amount of money")
 
-local cash = game.Players[PlayerName].Settings.Data.Cash.Value
+local cash = game.Players[PlayerName].Data.Cash.Value
 Section:NewLabel(cash)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 local Tab = Window:NewTab("Stand in storage")
-local Section = Tab:NewSection("")
+local Section = Tab:NewSection("Stand in storage")
 
 local StandInStorage1 = game.Players[PlayerName].Data["Ability Storage"].Ability_Slot1.Value
 local StandInStorage2 = game.Players[PlayerName].Data["Ability Storage"].Ability_Slot2.Value
@@ -38,7 +47,7 @@ Section:NewLabel(StandInStorage6)
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 local Tab = Window:NewTab("Item in storage")
-local Section = Tab:NewSection("")
+local Section = Tab:NewSection("Item in storage")
 
 local ItemInStorage1 = game.Players[PlayerName].Data["Item Storage"].Item_Slot1.Value
 local ItemInStorage2 = game.Players[PlayerName].Data["Item Storage"].Item_Slot2.Value
@@ -55,22 +64,8 @@ Section:NewLabel(ItemInStorage6)
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-local Tab = Window:NewTab("Settings")
-local Section = Tab:NewSection("")
-
-local Camera_Shake = game.Players[PlayerName].Settings["Camera Shake"].Value
-local CameraShake = game.Players[PlayerName].Settings.CameraShake.Value
-local LowGraphics = game.Players[PlayerName].Settings["Low Graphics"].Value
-local Music = game.Players[PlayerName].Settings.Music.Value
-Section:NewLabel(Camera_Shake)
-Section:NewLabel(CameraShake)
-Section:NewLabel(LowGraphics)
-Section:NewLabel(Music)
-
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-local Tab = Window:NewTab("Leaderstats")
-local Section = Tab:NewSection("")
+local Tab = Window:NewTab("Statistics")
+local Section = Tab:NewSection("Murder statistics")
 
 local leaderstats = game.Players[PlayerName].leaderstats["Total Kill"].Value
 Section:NewLabel(leaderstats)
