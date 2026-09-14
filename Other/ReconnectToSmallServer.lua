5f1815c5b8a07fb391bb647453b486cbfde46ba3
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2026-09-14 14:38:07 +0200

    Create ReconnectToSmallServer.lua

---
+++ Other/ReconnectToSmallServer.lua
@@ -0,0 +1,29 @@
+local function js(i)return game:GetService("HttpService"):JSONDecode(i)end
+
+local index = 0
+local maxplrs = 30
+local lowestID
+while index do
+    if index == 0 then index = "" end
+    local res
+    if syn then
+        res = syn.request{
+            Url = "https://games.roblox.com/v1/games/"..tostring(game.PlaceId).."/servers/Public?sortOrder=Asc&limit=100&cursor="..index,
+            Method = "GET"
+        }
+    else
+        res = http_request({Url = "https://games.roblox.com/v1/games/"..tostring(game.PlaceId).."/servers/Public?sortOrder=Asc&limit=100&cursor="..index, Method = "GET"})
+    end
+   
+    for i,v in next, js(res.Body).data do
+        if v.playing < maxplrs then
+            maxplrs = v.playing
+            print(maxplrs)
+            lowestID = v.id
+        end
+    end
+    index = js(res.Body).nextPageCursor
+    wait()
+end
+wait()
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,tostring(lowestID))
